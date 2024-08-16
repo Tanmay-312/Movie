ebb77ae3473bc2a70742330e6b997cc76fa6f53c
Tanmay-312 <jain2002tj@gmail.com>
2024-08-16 20:17:48 +0530

    Add images and solve issue with images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { useState } from 'react';
 import MovieList from './Components/MovieList';
 import MovieDetails from './Components/MovieDetails';
-import incep from './assets/images/Inception.png';
+
+import img1 from './assets/images/Inception.png'
+import img2 from './assets/images/Matrix.jpg'
+import img3 from './assets/images/Titanic.jpg'
+import img4 from './assets/images/Pursuit.jpg'
+import img5 from './assets/images/Arrival.jpg'
+import img6 from './assets/images/Interstellar.jpg'
+import img7 from './assets/images/Reptile.jpg'
+import img8 from './assets/images/Mind.jpg'
+
 import './App.css';
 
 const App = () => {
@@ -10,56 +19,56 @@ const App = () => {
       id: 1,
       title: 'Inception',
       description: 'A mind-bending thriller.',
-      imgSrc: incep,
+      imgSrc: img1,
       reviews: [{ text: 'Great movie!', id: 1 }],
     },
     {
       id: 2,
       title: 'The Matrix',
       description: 'A sci-fi classic.',
-      
+      imgSrc: img2,
       reviews: [{ text: 'Amazing effects!', id: 1 }],
     },
     {
       id: 3,
       title: 'Titanic',
       description: 'A Romantic Thriller Story.',
+      imgSrc: img3,
       reviews: [{ text: 'Great movie!', id: 1 }],
     },
     {
       id: 4,
       title: 'The Pursuit of Happyness',
       description: 'A Fathers Love towards his Son Through all the hardships',
+      imgSrc: img4,
       reviews: [{ text: 'Great movie!', id: 1 }],
     },
     {
       id: 5,
       title: 'Arrival',
       description: 'Sci Fi Based',
+      imgSrc: img5,
       reviews: [{ text: 'Great movie!', id: 1 }],
     },
     {
     id: 6,
     title: 'Interstellar',
     description: 'Time Travel',
+    imgSrc: img6,
     reviews: [{ text: 'Great movie!', id: 1 }],
     },
     {
       id: 7,
-      title: 'Arrival',
-      description: 'Sci Fi Based',
+      title: 'Reptile',
+      description: 'Suspensefull',
+      imgSrc: img7,
       reviews: [{ text: 'Great movie!', id: 1 }],
     },
     {
       id: 8,
-      title: 'Reptile',
-      description: 'Suspensefull',
-      reviews: [{ text: 'Great movie!', id: 1 }],
-    },
-    {
-      id: 9,
       title: 'A Beautiful Mind',
       description: 'Biopics',
+      imgSrc: img8,
       reviews: [{ text: 'Great movie!', id: 1 }],
     },
   ]);
